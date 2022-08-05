1769500a36888953fec05eb0713ad42a68eea647
KavinSankar123 <90232501+KavinSankar123@users.noreply.github.com>
2022-08-04 17:07:07 -0700

    Create Solution.java

---
+++ Solutions/Easy/LeetCode242/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        
+        int[] characters = new int[26];
+        
+        
+        for(int i = 0; i < s.length(); i++) {
+            characters[s.charAt(i) - 'a']++;
+            characters[t.charAt(i) - 'a']--;
+        }
+        
+        for(int i: characters) {
+            if (i != 0)
+                return false;
+        }
+        
+        
+        return true;
+    }  
+}
