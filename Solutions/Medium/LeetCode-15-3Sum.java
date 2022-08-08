0de0b9bead3a5894853e81f47b210630810ab621
KavinSankar123 <90232501+KavinSankar123@users.noreply.github.com>
2022-08-08 15:41:40 -0700

    Create LeetCode-15-3Sum.java

---
+++ Solutions/Medium/LeetCode-15-3Sum.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        List<List<Integer>> groups = new ArrayList<List<Integer>>(); // lists for holding all triplets
+
+        for(int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int left = i + 1;
+                int right = nums.length - 1;
+                
+                while (left < right) {
+                    int diff = nums[i] + nums[left] + nums[right];
+
+                    if (diff == 0) {
+                        List<Integer> triplet = new ArrayList<>(); // list for holding a single triplet
+                        
+                        triplet.add(nums[i]);
+                        triplet.add(nums[left]);
+                        triplet.add(nums[right]); 
+                        groups.add(triplet);
+                        
+                        while (left < right && nums[left] == nums[left + 1]) 
+                                left++;
+                        
+                        while (left < right && nums[right] == nums[right - 1]) 
+                                right--;
+                        
+                        left++;
+                        right--;
+                    }
+                    else if (diff < 0) {
+                        left++;
+                    }
+                    else {
+                        right--;
+                    }
+                }
+            }
+            
+
+        }
+        return groups;
+    }
+}
